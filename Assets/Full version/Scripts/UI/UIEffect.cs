154a4d0015d790f552116f282138f8b832e31d11
김은총 <eunchonggim999@gmail.com>
2023-06-22 22:11:12 +0900

    UI 이펙트 구현

---
+++ Assets/Full version/Scripts/UI/UIEffect.cs
@@ -0,0 +1,26 @@
+using DG.Tweening;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class UIEffect : MonoBehaviour
+{
+    [SerializeField] private float fadeTime = 1f;
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private RectTransform rectTransform;
+
+    public void PanelFadeIn()
+    {
+        canvasGroup.alpha = 0f;
+        rectTransform.transform.localPosition = new Vector3 (0f, -1000f, 0f);
+        rectTransform.DOAnchorPos(new Vector2(0f, 0f), fadeTime, false).SetEase(Ease.OutElastic);
+        canvasGroup.DOFade(1, fadeTime);
+    }
+
+    public void PanelFadeOut()
+    {
+        canvasGroup.alpha = 1f;
+        rectTransform.transform.localPosition = new Vector3(0f, 0f, 0f);
+        rectTransform.DOAnchorPos(new Vector2(0f, -1000f), fadeTime, false).SetEase(Ease.InOutQuint);
+        canvasGroup.DOFade(0, fadeTime);
+    }
+}
