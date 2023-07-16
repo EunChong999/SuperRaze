7813a56af4a7950d167d4b97f418111aef5cb5c2
김은총 <eunchonggim999@gmail.com>
2023-07-16 23:58:18 +0900

    FEAT : Camera Shaking Implemented

---
+++ Assets/Full version/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,46 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public static CameraShake instance { get; private set; }
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private float shakeTimer;
+    private float shakeTimerTotal;
+    private float startingIntensity;
+
+    private void Awake()
+    {
+        instance = this;
+        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void ShakeCamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+            cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>(); ;
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+
+        startingIntensity = intensity;
+        shakeTimerTotal = time;
+        shakeTimer = time;
+    }
+
+    private void Update()
+    {
+        if (shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+
+            if (shakeTimer <= 0f)
+            {
+                // 타임 오버
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+                        cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = Mathf.Lerp(startingIntensity, 0f, shakeTimer / shakeTimerTotal);
+            }
+        }
+    }
+}
