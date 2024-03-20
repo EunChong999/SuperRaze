c0de3833f1b97e96dcf3b0268caf7e7c58fa3165
EunChong999 <eunchonggim999@gmail.com>
2024-03-20 14:35:29 +0900

    fix : 수정

--- Assets/Full version/Scripts/Player/Skill/PlayerTimeRewinder.cs
+++ Assets/Full version/Scripts/Player/Skill/PlayerTimeRewinder.cs
@@ -75,28 +75,7 @@ public class PlayerTimeRewinder : MonoBehaviour
     {
         if (playerPointTime.Count > 0)
         {
-            curTime += Time.deltaTime;
-
-            if (recordTime - curTime <= 0)
-            {
-                StopRewind();
-            }
-            else
-            {
-                float timeSinceLerpStart = Time.time - lerpStartTime;
-                float t = Mathf.Clamp01(timeSinceLerpStart / (lerpDuration / rewindSpeed)); // rewindSpeed로 나누어 보정
-                body.transform.position = Vector3.Lerp(previousPosition, targetPosition, t);
-
-                if (t >= 1f)
-                {
-                    PlayerPointTime pointInTime = playerPointTime.Pop();
-                    targetPosition = pointInTime.position;
-                    body.transform.rotation = pointInTime.rotation;
-                    previousPosition = body.transform.position;
-                    lerpStartTime = Time.time;
-                    lerpDuration = Vector3.Distance(previousPosition, targetPosition) / rewindSpeed;
-                }
-            }
+            DoRewind();
         }
         else
         {
@@ -104,12 +83,37 @@ public class PlayerTimeRewinder : MonoBehaviour
         }
     }
 
+    void DoRewind()
+    {
+        float timeSinceLerpStart = Time.time - lerpStartTime;
+        float t = Mathf.Clamp01(timeSinceLerpStart / (lerpDuration / rewindSpeed)); // rewindSpeed로 나누어 보정
+        body.transform.position = Vector3.Lerp(previousPosition, targetPosition, t);
+
+        if (t >= 1f)
+        {
+            PlayerPointTime pointInTime = playerPointTime.Pop();
+            targetPosition = pointInTime.position;
+            body.transform.rotation = pointInTime.rotation;
+            previousPosition = body.transform.position;
+            lerpStartTime = Time.time;
+            lerpDuration = Vector3.Distance(previousPosition, targetPosition) / rewindSpeed;
+        }
+    }
 
     void Record()
     {
-        if (rb.velocity.x != 0 && rb.velocity.y != 0)
+        curTime += Time.deltaTime;
+
+        if (curTime >= recordTime)
         {
-            playerPointTime.Push(new PlayerPointTime(body.transform.position, body.transform.rotation));
+            playerPointTime.Pop();
+        }
+        else
+        {
+            if (rb.velocity.x != 0 && rb.velocity.y != 0)
+            {
+                playerPointTime.Push(new PlayerPointTime(body.transform.position, body.transform.rotation));
+            }
         }
     }
 
